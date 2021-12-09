181ce0ed36fec7a978a255cb0fdb34560d51e8d1
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-12-09 16:08:59 -0300

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+import altair as alt
+# import plotly.express as px
+# import plotly.graph_objects as go
+
+import streamlit as st
+
+# -------------------- #
+# ------ GRAPHS ------ #
+# -------------------- #
+
+
+# -------------------- #
+# ----- INTERFACE----- #
+# -------------------- #
+
+st.title("Chess Data Vis")
